a0e9a6bd664062ba4f75c172f52128b6485f3911
YanBerdin <yandevformation@gmail.com>
2025-08-23 00:07:04 +0200

    feat(Partners) added partners section to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ import { Hero } from '@/components/sections/hero';
 import { FeaturedNews } from '@/components/sections/featured-news';
 import { UpcomingShows } from '@/components/sections/upcoming-shows';
 import { Newsletter } from '@/components/sections/newsletter';
+import { Partners } from '@/components/sections/partners';
 
 export default function Home() {
   return (
@@ -21,6 +22,7 @@ export default function Home() {
       <AboutPreview />
       <UpcomingShows />
       <Newsletter />
+      <Partners />
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <main className="flex-1 flex flex-col gap-6 px-4">
