7163a8dcbed961bc348f27c0ad4a99c473300406
YanBerdin <yandevformation@gmail.com>
2025-12-07 01:56:11 +0100

    chore(admin layout) remove fragment

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -39,36 +39,34 @@ export default async function AdminLayout({
   }
 
   return (
-    <>
+    <SidebarProvider>
       <BfcacheHandler />
-      <SidebarProvider>
-        <AppSidebar hasEnvVars={!!hasEnvVars} />
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="/admin">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-            <div className="ml-auto">
-              <ThemeSwitcher />
-            </div>
-          </header>
-          <div className="flex flex-1 flex-col gap-4 p-4">
-            {children}
+      <AppSidebar hasEnvVars={!!hasEnvVars} />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="/admin">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+          <div className="ml-auto">
+            <ThemeSwitcher />
           </div>
-        </SidebarInset>
-      </SidebarProvider>
-    </>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
